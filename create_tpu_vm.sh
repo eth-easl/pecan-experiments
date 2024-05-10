304cd49b3b08083cd98268e784577c74c5effb84
DanGraur <dan.graur@inf.ethz.ch>
2024-05-10 18:08:36 +0200

    Fix error message in create_tpu_vm

--- create_tpu_vm.sh
+++ create_tpu_vm.sh
@@ -6,7 +6,7 @@ vm_name="atc24-ae-${my_user}"
 while ! gcloud alpha compute tpus tpu-vm create --zone=europe-west4-a --accelerator-type="v2-8" --version='tpu-vm-tf-2.8.0' ${vm_name}; do echo "Trying again..."; done
 
 if [ "$?" -ne 0 ]; then
-  echo "Successfully created TPU VM ${vm_name}"
-else
   echo "Could not create TPU VM ${vm_name}"
+else
+  echo "Successfully created TPU VM ${vm_name}"
 fi
