b8a5c2ddcb303521d5e2493d5f75db79a4c893a0
omrazCZ <oto.mraz@gmail.com>
2024-05-12 16:50:03 +0200

    Parameterize experiment script

--- experiments/pecan/README.md
+++ experiments/pecan/README.md
@@ -4,7 +4,7 @@ All of the below commands are intended to be executed on the remote TPU VM as de
 2. **Activate tmux window**. Since some of our experiments can take long to run, make sure to setup a terminal multiplexer such as `tmux` in case your connection is interrupted. A very short introduction can be found at the end of this readme.
 3. **Starting the cluster**. Execute `./manage_cluster.sh start`. The script will create and setup a cluster of several virtual machines.
 4. **Checking the status** of the cluster by executing `./manage_cluster.sh status`. If all the status indicators show a green `[OK]`, carry on with the next step.
-5. **Run ResNet50 experiment**. Run the following command to exectue the experiments for the ResNet in Fig 8. `python run_fig8.py`. More concretely this script does the following:
+5. **Run ResNet50 experiment**. Run the following command to exectue the experiments for the ResNet in Fig 8. `python run_fig8.py ResNet50_v2-8`. More concretely this script does the following:
     a. **Running the input pipeline with Pecan** - producing data for the brown bars
     b. **Runnign the input pipeline with Cachew** - producing data for the orange bars
     c. **Runing the pipeline in the collocated mode** - producing data for the blue bars
--- experiments/pecan/run_fig8.py
+++ experiments/pecan/run_fig8.py
@@ -1,7 +1,19 @@
-import os, sys
+import os
 import subprocess as sp
 from subprocess import Popen, PIPE
 import shlex
+import argparse
+
+parser = argparse.ArgumentParser(
+        add_help=True,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+parser.add_argument('-m', '--model', type=str, help='model used', default='ResNet50_v2-8')
+
+args = parser.parse_args()
+
+model = args.model
 
 '''
     a. **Running the input pipeline with Pecan** - producing data for the brown bars
@@ -39,9 +51,9 @@ EVAL_FILE_PATTERN="$DATA_DIR/val-*"
 VAL_JSON_FILE="gs://tfdata-datasets-eu/coco/raw-data/annotations/instances_val2017.json"
 RESNET_CHECKPOINT="gs://cloud-tpu-checkpoints/retinanet/resnet50-checkpoint-2018-02-07"
 ITERS_PER_LOOP=1848 # 1848 steps = 1 epoch
-epochs=72 # default: 13, 72 is 2nd version
+epochs=$num_epochs # default: 13, 72 is 2nd version
 total_steps=$(($epochs * $ITERS_PER_LOOP))
-eval=true
+eval=false
 save_checkpoint_freq=1
 model_dir="gs://otmraz-eu-logs/Retinanet/${USER}"
 log_out="main.log"
@@ -52,29 +64,52 @@ pecan_cmd = '''
 export USE_AUTOORDER=True
 export n_loc=10
 export DISPATCHER_IP='disp'
-log_out="../../../../../../../../pecan-experiments/experiments/pecan/logs/pecan.log"
+log_out="../../../../../../../../pecan-experiments/experiments/pecan/logs/resnet_pecan.log"
 '''
 cachew_cmd = '''
 export USE_AUTOORDER=False
 export n_loc=0
 export DISPATCHER_IP='disp'
-log_out="../../../../../../../../pecan-experiments/experiments/pecan/logs/cachew.log"
+log_out="../../../../../../../../pecan-experiments/experiments/pecan/logs/resnet_cachew.log"
 '''
 colloc_cmd = '''
 export USE_AUTOORDER=False
 export n_loc=0
 export DISPATCHER_IP='None'
-log_out="../../../../../../../../pecan-experiments/experiments/pecan/logs/colloc.log"
+log_out="../../../../../../../../pecan-experiments/experiments/pecan/logs/resnet_colloc.log"
+'''
+
+pecan_retina_cmd = '''
+export USE_AUTOORDER=True
+export n_loc=10
+export DISPATCHER_IP='disp'
+num_epochs={0}
+log_out="../../../../../../../pecan-experiments/experiments/pecan/logs/retina_pecan.log"
+'''
+cachew_retina_cmd = '''
+export USE_AUTOORDER=False
+export n_loc=0
+export DISPATCHER_IP='disp'
+num_epochs={0}
+log_out="../../../../../../../pecan-experiments/experiments/pecan/logs/retina_cachew.log"
+'''
+colloc_retina_cmd = '''
+export USE_AUTOORDER=False
+export n_loc=0
+export DISPATCHER_IP='None'
+num_epochs={0}
+log_out="../../../../../../../pecan-experiments/experiments/pecan/logs/retina_colloc.log"
 '''
 
 exp_dir = '''../../../../../../../../pecan-experiments/experiments/pecan''' # From resnet dir
+exp_dir_from_retina = '''../../../../../../../pecan-experiments/experiments/pecan''' # From resnet dir
 resnet_dir = '''../../../ml_input_processing/experiments/ml/models/official/vision/image_classification/resnet/'''
 retina_dir = '''../../../ml_input_processing/experiments/ml/models/official/vision/detection/'''
 
-ResNet_cmd = '''python3 resnet_ctl_imagenet_main.py --enable_checkpoint_and_export=true --tpu=$tpu_name --model_dir=$model_dir --data_dir=$data_dir --batch_size=1024 --steps_per_loop=500 --train_epochs=$train_epochs --use_synthetic_data=false --dtype=fp32 --enable_eager=true --enable_tensorboard=true --distribution_strategy=tpu --log_steps=50 --single_l2_loss_op=true --verbosity=0 --skip_eval=true --use_tf_function=true --num_local_workers=$n_loc 2>&1 | tee $log_out'''
 ResNet_cmd_param = '''python3 resnet_ctl_imagenet_main.py --enable_checkpoint_and_export=true --tpu=$tpu_name --model_dir=$model_dir --data_dir=$data_dir --batch_size=1024 --steps_per_loop={0} --train_epochs={1} --use_synthetic_data=false --dtype=fp32 --enable_eager=true --enable_tensorboard=true --distribution_strategy=tpu --log_steps=50 --single_l2_loss_op=true --verbosity=0 --skip_eval=true --use_tf_function=true --num_local_workers=$n_loc 2>&1 | tee $log_out'''
-Retina_cmd = '''python main.py --strategy_type=tpu --tpu="${TPU_ADDRESS?}" --model_dir="${model_dir?}" --save_checkpoint_freq=$save_checkpoint_freq --mode=train --local_workers=$n_loc --params_override="{ type: retinanet, train: { checkpoint: { path: ${RESNET_CHECKPOINT?}, prefix: resnet50/ }, train_file_pattern: ${TRAIN_FILE_PATTERN?}, iterations_per_loop: ${ITERS_PER_LOOP}, total_steps: ${total_steps}}, eval: { val_json_file: ${VAL_JSON_FILE?}, eval_file_pattern: ${EVAL_FILE_PATTERN?}, num_steps_per_eval: ${ITERS_PER_LOOP} } }" 2>&1 | tee $log_out
-'''
+Retina_cmd = '''python main.py --strategy_type=tpu --tpu="${TPU_ADDRESS?}" --model_dir="${model_dir?}" --save_checkpoint_freq=$save_checkpoint_freq --mode=train --local_workers=$n_loc --params_override="{ type: retinanet, train: { checkpoint: { path: ${RESNET_CHECKPOINT?}, prefix: resnet50/ }, train_file_pattern: ${TRAIN_FILE_PATTERN?}, iterations_per_loop: ${ITERS_PER_LOOP}, total_steps: ${total_steps}}, eval: { val_json_file: ${VAL_JSON_FILE?}, eval_file_pattern: ${EVAL_FILE_PATTERN?}, num_steps_per_eval: ${ITERS_PER_LOOP} } }" 2>&1 | tee $log_out'''
+# This won't work
+retina_cmd_param = '''python main.py --strategy_type=tpu --tpu="${TPU_ADDRESS?}" --model_dir="${model_dir?}" --save_checkpoint_freq=$save_checkpoint_freq --mode=train --local_workers=$n_loc --params_override="{ type: retinanet, train: { checkpoint: { path: ${RESNET_CHECKPOINT?}, prefix: resnet50/ }, train_file_pattern: ${TRAIN_FILE_PATTERN?}, iterations_per_loop: 250, total_steps: ${total_steps}}, eval: { val_json_file: ${VAL_JSON_FILE?}, eval_file_pattern: ${EVAL_FILE_PATTERN?}, num_steps_per_eval: ${ITERS_PER_LOOP} } }" 2>&1 | tee $log_out'''
 
 cost_extract_cmd = '''python plotting-scripts/extract_costs.py --path={0} --model={1} --accelerator=v2 --experiment_type={2} --header=True'''
 
@@ -122,53 +157,101 @@ def set_service_img(img):
 
 disp_ip = get_disp_ip()
 
-### a) Pecan
+if model == 'ResNet50_v2-':
+    print('Running Resnet experiments')
+    ### a) Pecan
 
-n_local = 10
-n_steps = 5000
-set_service_img(pecan_img)
+    n_local = 10
+    n_steps = 5000
+    set_service_img(pecan_img)
 
-sp.run(restart_workers_cmd, shell=True)
-os.chdir(resnet_dir)
-sp.run(prepare_resnet_cmd+pecan_cmd+ResNet_cmd_param.format(500, 8), shell=True)
-os.chdir(exp_dir)
-sp.run('gsutil rm -r '+resnet_model_dir, shell=True)
+    sp.run(restart_workers_cmd, shell=True)
+    os.chdir(resnet_dir)
+    sp.run(prepare_resnet_cmd+pecan_cmd+ResNet_cmd_param.format(500, 8), shell=True)
+    os.chdir(exp_dir)
+    sp.run('gsutil rm -r '+resnet_model_dir, shell=True)
 
-### b) Cachew
+    ### b) Cachew
 
-n_local = 0
-n_steps = 5000
-set_service_img(cachew_img)
+    n_local = 0
+    n_steps = 5000
+    set_service_img(cachew_img)
 
-sp.run(restart_workers_cmd, shell=True)
-os.chdir(resnet_dir)
-sp.run(prepare_resnet_cmd+cachew_cmd+ResNet_cmd_param.format(500, 4), shell=True)
-os.chdir(exp_dir)
-sp.run(stop_workers_cmd, shell=True)
-sp.run('gsutil rm -r '+resnet_model_dir, shell=True)
+    sp.run(restart_workers_cmd, shell=True)
+    os.chdir(resnet_dir)
+    sp.run(prepare_resnet_cmd+cachew_cmd+ResNet_cmd_param.format(500, 4), shell=True)
+    os.chdir(exp_dir)
+    sp.run(stop_workers_cmd, shell=True)
+    sp.run('gsutil rm -r '+resnet_model_dir, shell=True)
 
-### c) No service
+    ### c) No service
 
-n_local = 0
-n_steps = 5000
-disp_ip = 'None'
+    n_local = 0
+    n_steps = 5000
+    disp_ip = 'None'
 
-os.chdir(resnet_dir)
-sp.run(prepare_resnet_cmd+colloc_cmd+ResNet_cmd_param.format(250, 2), shell=True)
-os.chdir(exp_dir)
-sp.run('gsutil rm -r '+resnet_model_dir, shell=True)
+    os.chdir(resnet_dir)
+    sp.run(prepare_resnet_cmd+colloc_cmd+ResNet_cmd_param.format(250, 2), shell=True)
+    os.chdir(exp_dir)
+    sp.run('gsutil rm -r '+resnet_model_dir, shell=True)
 
-### d) Plotting
-os.chdir(exp_dir)
-# TODO: Replace the paths!!!!
-_, pecan_out, _ = get_exitcode_stdout_stderr(cost_extract_cmd.format('logs/pecan.log', 'resnet', 'pecan'))
-_, cachew_out, _ = get_exitcode_stdout_stderr(cost_extract_cmd.format('logs/cachew.log', 'resnet', 'cachew'))
-_, colloc_out, _ = get_exitcode_stdout_stderr(cost_extract_cmd.format('logs/colloc.log', 'resnet', 'collocated'))
+    ### d) Plotting
+    os.chdir(exp_dir)
+    _, pecan_out, _ = get_exitcode_stdout_stderr(cost_extract_cmd.format('logs/resnet_pecan.log', 'resnet', 'pecan'))
+    _, cachew_out, _ = get_exitcode_stdout_stderr(cost_extract_cmd.format('logs/resnet_cachew.log', 'resnet', 'cachew'))
+    _, colloc_out, _ = get_exitcode_stdout_stderr(cost_extract_cmd.format('logs/resnet_colloc.log', 'resnet', 'collocated'))
 
-pecan_tpu, pecan_cpu = get_costs(pecan_out.decode("utf-8"))
-cachew_tpu, cachew_cpu = get_costs(cachew_out.decode("utf-8"))
-colloc_tpu, colloc_cpu = get_costs(colloc_out.decode("utf-8"))
+    pecan_tpu, pecan_cpu = get_costs(pecan_out.decode("utf-8"))
+    cachew_tpu, cachew_cpu = get_costs(cachew_out.decode("utf-8"))
+    colloc_tpu, colloc_cpu = get_costs(colloc_out.decode("utf-8"))
 
-_, _, _ = get_exitcode_stdout_stderr(plot_cmd.format('ResNet50_v2-8', ' '.join([colloc_tpu, cachew_tpu, pecan_tpu]), ' '.join([colloc_cpu, cachew_cpu, pecan_cpu])))
+    _, _, _ = get_exitcode_stdout_stderr(plot_cmd.format('ResNet50_v2-8', ' '.join([colloc_tpu, cachew_tpu, pecan_tpu]), ' '.join([colloc_cpu, cachew_cpu, pecan_cpu])))
+
+elif model == 'retina':
+    print('Running Retina experiments')
+    ### a) Pecan
+
+    n_local = 10
+    set_service_img(pecan_img)
+
+    sp.run(restart_workers_cmd, shell=True)
+    os.chdir(retina_dir)
+    sp.run(pecan_retina_cmd.format(10)+prepare_retina_cmd+retina_cmd_param, shell=True)
+    os.chdir(exp_dir_from_retina)
+    sp.run('gsutil rm -r '+retina_model_dir, shell=True)
+
+    ### a) Cachew
+
+    n_local = 0
+    set_service_img(cachew_img)
+
+    sp.run(restart_workers_cmd, shell=True)
+    os.chdir(retina_dir)
+    sp.run(cachew_retina_cmd.format(8)+prepare_retina_cmd+retina_cmd_param, shell=True)
+    os.chdir(exp_dir_from_retina)
+    sp.run(stop_workers_cmd, shell=True)
+    sp.run('gsutil rm -r '+retina_model_dir, shell=True)
+
+    ### c) No service
+
+    n_local = 0
+    disp_ip = 'None'
+
+    os.chdir(retina_dir)
+    sp.run(colloc_retina_cmd.format(2)+prepare_retina_cmd+retina_cmd_param, shell=True) # the param is actually the number of steps (here 2 epochs)
+    os.chdir(exp_dir_from_retina)
+    sp.run('gsutil rm -r '+retina_model_dir, shell=True)
+
+    ### d) Plotting
+    os.chdir(exp_dir)
+    _, pecan_out, _ = get_exitcode_stdout_stderr(cost_extract_cmd.format('logs/resnet_pecan.log', 'resnet', 'pecan'))
+    _, cachew_out, _ = get_exitcode_stdout_stderr(cost_extract_cmd.format('logs/resnet_cachew.log', 'resnet', 'cachew'))
+    _, colloc_out, _ = get_exitcode_stdout_stderr(cost_extract_cmd.format('logs/sample_logs/retina_colloc.log', 'resnet', 'collocated'))
+
+    pecan_tpu, pecan_cpu = get_costs(pecan_out.decode("utf-8"))
+    cachew_tpu, cachew_cpu = get_costs(cachew_out.decode("utf-8"))
+    colloc_tpu, colloc_cpu = get_costs(colloc_out.decode("utf-8"))
+
+    _, _, _ = get_exitcode_stdout_stderr(plot_cmd.format('ResNet50_v2-8', ' '.join([colloc_tpu, cachew_tpu, pecan_tpu]), ' '.join([colloc_cpu, cachew_cpu, pecan_cpu])))
 
 print("Finished experiments!")
